2e0f202ddf960b0123ebe03d3e9cf23e7b865f4a
etudiant <etudiant@3il-1a-java-vm>
2023-03-31 08:44:13 +0200

    Mise en place du projet

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,7 +3,9 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.fxml.FXMLLoader;
 
 
@@ -12,9 +14,17 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("IHM.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,500,400);
+			scene.setFill(Color.LIGHTGRAY);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Gestionnaire de couleurs");
+			primaryStage.setResizable(false);
+			
+			Image icone3iL = new Image("ressources/logo_3iL.png");
+			primaryStage.getIcons().add(icone3iL);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
